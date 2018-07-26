6b46ca4e12e9f9f2cd693258b17aabc1757e83b2
jim.mcquarrie <jim.mcquarrie@iceenterprise.com>
2018-07-26 12:37:42 -0700

    Initial Commit

---
+++ MateApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using MateApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MateApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
